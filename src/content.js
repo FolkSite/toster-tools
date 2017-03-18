38e35be095936a10f33123e9e0af8eee7195d10d
yarkovaleksei <yarkov_aleksei@mail.ru>
2017-03-19 00:20:00 +0300

    Fix error of version 1.1

--- src/content.js
+++ src/content.js
@@ -11,12 +11,22 @@ if ( !!commentForms ) {
 
     const content = fs.readFileSync( __dirname + '/twpwyg.html', 'utf-8' );
 
+    const createElement = ( str ) => {
+        let elem = document.createElement( 'div' );
+        elem.innerHTML = str;
+        if ( elem.childNodes.length > 0 ) {
+            return elem.childNodes[ 0 ];
+        }
+        return elem;
+    }
+
+
     for ( let i = 0; i < commentForms.length; i++ ) {
         let form = commentForms[ i ];
         let field_wrap = form.querySelector( 'div.field_wrap' );
 
         let div = document.createElement( 'div' );
-        div.appendChild( document.createTextNode( content ) );
+        div.appendChild( createElement( content ) );
         field_wrap.insertBefore( div, field_wrap.firstChild );
     }
 }
